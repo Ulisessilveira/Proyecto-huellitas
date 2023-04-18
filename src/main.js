ab6c8a2676ff8cc73227fff777f37b1bd791dd14
Ulises Silveira <71781728+Ulisessilveira@users.noreply.github.com>
2023-04-18 12:15:40 -0600

    update ulises
    si

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import router from './routes'
 
 const app = createApp(App)
