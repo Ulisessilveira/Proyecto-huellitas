ffdceb0567ec96befa92863ef415a1d08905ee3b
Ulises Silveira <71781728+Ulisessilveira@users.noreply.github.com>
2023-05-03 10:23:12 -0600

    update ulises

--- frontend/src/views/front/details/DetailsComponent.vue
+++ frontend/src/views/front/details/DetailsComponent.vue
@@ -5,15 +5,15 @@
         <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" :src="url+'img/products/'+data.img" alt="..." /></div>
                     <div class="col-md-6">
                         <div class="small mb-1">SKU: BST-498</div>
-                        <h1 class="display-5 fw-bolder">Shop item template</h1>
+                        <h1 class="display-5 fw-bolder">{{ data.name }}</h1>
                         <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">$45.00</span>
-                            <span>$40.00</span>
+                            <span class="text-decoration-line-through">{{data.price + 1.2}}</span>
+                            &nbsp;<span>{{data.price}}</span>
                         </div>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                        <p class="lead">{{ data.description }}</p>
                         <div class="d-flex">
                             <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
                             <button class="btn btn-outline-dark flex-shrink-0" type="button">
@@ -137,13 +137,26 @@
 
 <script>
     import NavFrontComponent from '../../../components/layoutsfrontend/NavFrontComponent.vue'
+    import axios from 'axios'
     export default{
         name:'DetailsComponent',
         components:{
             NavFrontComponent
         },
+        data(){
+            return{
+                data:[],
+                url:""
+            }
+        },
         created(){
+            this.url = process.env.VUE_APP_IMG
+            axios.get(process.env.VUE_APP_URL+"products/"+this.$route.params.slug).then(res=>{
+                this.data = res.data.data
+            })
             console.log(this.$route.params.slug)
         }
     }
+    
+    
 </script>
