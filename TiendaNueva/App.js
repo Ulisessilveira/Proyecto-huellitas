aed191f2d3e58628e84f0f58ad81903b872dedfc
Saul Eduardo Rodríguez Prieto <71780702+Insanity16@users.noreply.github.com>
2023-05-08 13:40:40 -0600

    React

--- TiendaNueva/App.js
+++ TiendaNueva/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginComponent from "./views/auth/LoginComponent";
+import HomeComponent from "./views/Home/HomeComponent";
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Screen component={LoginComponent} name="Login"/>
+      <Stack.Screen component={HomeComponent} name="Home"/>
+
+    </NavigationContainer>
+    
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ TiendaNueva/views/auth/LoginComponent.js
@@ -0,0 +1,9 @@
+import {View,Text,Button} from"react-native"
+export default function LoginComponent() {
+  return (
+    <View>
+        <Text> Login </Text>
+        <Button title="Ingresar" color="#555555" onPress={()=> {}}/>
+    </View>
+  )
+}
